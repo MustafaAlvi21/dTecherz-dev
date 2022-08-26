51689b464548b88386eaacfa03f01138044364d4
MustafaAlvi21 <mustafaalvi21@gmail.com>
2022-08-26 20:34:48 +0500

    first commit

---
+++ JS/26-08-2022.js
@@ -0,0 +1,50 @@
+// Task
+// Do practice of variables and all data types, JSON, if else (with all conditions) and simple for loop
+// ++(increament), --(decreament)
+
+// ---------------------------------------------------------
+
+// name = "Mustafa"
+// age = 1
+// yesNo = "ture"
+// arr = ["Ali", 1233, yesNo, name, false, age, [1, 2, 4, 5, name], {
+//     name: name,
+//     age: 132
+// }]
+
+// obj = {
+// //  key: value,
+//     name: name,
+//     age: 132,
+// }
+
+// console.log(name, age, 123, "asdsad", obj);
+// console.log(obj);
+
+// myData = [
+//     {
+//         name: "ALi"
+//     },{
+//         name: "Sara"
+//     },{
+//         name
+//     }
+// ]
+
+// console.log(myData);
+
+
+
+
+// if (age >= 3 ) {
+//     console.log(true);
+
+// } 
+    
+    // 0 < 2 = true
+for (i=0; i < 2; i++) {
+    console.log(i);    
+    i-=2
+    console.log(i);    
+}
+
